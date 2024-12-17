a2ccbd6ce45bb97e60f86109b0deaa6a17f60e47
batuhangozcu <batuhangozcu1@gmail.com>
2024-12-17 08:48:22 +0300

    taskTwo

--- js/task-2.js
+++ js/task-2.js
@@ -11,16 +11,18 @@ const images = [
     url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
     alt: "Group of Horses Running",
   },
-  {
-    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
-    alt: "Alpine Spring Meadows",
-  },
-  {
-    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
-    alt: "Nature Landscape",
-  },
-  {
-    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
-    alt: "Lighthouse Coast Sea",
-  }
 ];
+
+const list = document.querySelector(".gallery");
+// const item1 = document.createElement("li")
+// const item2 = document.createElement("li")
+// const item3 = document.createElement("li")
+// list.appendChild(item1)
+// list.appendChild(item2)
+// list.appendChild(item3)
+
+const image = images.map(
+  (obj) => `<li><img src="${obj.url}" alt="${obj.alt}">`
+).join("");
+
+list.insertAdjacentHTML("afterbegin", image);
