1f03503434cf95ccc040efc1ebfae6669b6da127
batuhangozcu <batuhangozcu1@gmail.com>
2024-12-17 07:30:54 +0300

    taskOne

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,11 @@
-const categories = document.querySelector("#categories")
+const categories = document.querySelector("#categories");
 console.log(`Number of categories: ${categories.children.length}`);
+
+const item = document.querySelectorAll(".item");
+
+item.forEach((items) => {
+  const categoryName = items.querySelector("h2").textContent;
+  console.log(`Category: ${categoryName}`);
+  const elements = items.querySelectorAll("ul li").length;
+  console.log(`Elements: ${elements}`);
+});
