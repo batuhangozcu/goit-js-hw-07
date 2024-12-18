976c91a5724b40678e9659294036daf2ad710868
batuhangozcu <batuhangozcu1@gmail.com>
2024-12-19 01:17:44 +0300

    taskFive

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+
+button.addEventListener("click", (e) => {
+  document.querySelector("body").style.backgroundColor = getRandomHexColor();
+  document.querySelector(".color").textContent =
+    document.querySelector("body").style.backgroundColor;
+});
