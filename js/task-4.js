eece3d09ba317adcee12ea5c6bb7da82343fee09
batuhangozcu <batuhangozcu1@gmail.com>
2024-12-19 01:01:18 +0300

    taskFour

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
+const mail = document.querySelector("input[name=email]");
+const pass = document.querySelector("input[name=password]");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let userData = {
+    email: mail.value.trim(),
+    password: pass.value.trim(),
+  };
+
+  if (userData.email == "") {
+    alert("All form fields must be filled in");
+    return
+  }
+  if (userData.password == "") {
+    alert("All form fields must be filled in");
+    return
+  }
+  console.log(userData);
+  form.reset()
+});
